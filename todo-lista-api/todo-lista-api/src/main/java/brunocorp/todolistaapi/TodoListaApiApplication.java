5fe73067fa699fc5402e65d38048309872ca2ff9
BrunoSantosdeOliveira <84398016+BrunoSantosdeOliveira@users.noreply.github.com>
2021-07-23 17:51:44 -0300

    apilogin
    projeto spring boot

---
+++ todo-lista-api/todo-lista-api/src/main/java/brunocorp/todolistaapi/TodoListaApiApplication.java
@@ -0,0 +1,13 @@
+package brunocorp.todolistaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListaApiApplication.class, args);
+	}
+
+}
