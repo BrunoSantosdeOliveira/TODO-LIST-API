08d5e60b047e962aada5ec531668d6c7f42aef92
BrunoSantosdeOliveira <84398016+BrunoSantosdeOliveira@users.noreply.github.com>
2021-07-27 17:58:11 -0300

    Junit
    testes unitarios

--- agenda/agenda/src/test/java/brunocorp/agenda/AgendaApplicationTests.java
+++ agenda/agenda/src/test/java/brunocorp/agenda/AgendaApplicationTests.java
@@ -1,13 +1,10 @@
 package brunocorp.agenda;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AgendaApplicationTests {
-
+public class AgendaApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
--- todo-lista-api/todo-lista-api/src/main/java/brunocorp/todolistaapi/controller/UsuarioController.java
+++ todo-lista-api/todo-lista-api/src/main/java/brunocorp/todolistaapi/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import brunocorp.todolistaapi.repository.UsuarioRepository;
@@ -21,4 +22,6 @@ public class UsuarioController {
     			 .map(record -> ResponseEntity.ok().body(record))
     			 .orElse(ResponseEntity.notFound().build());
      }
+
+
 }
